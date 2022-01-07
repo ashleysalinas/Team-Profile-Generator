7cb2387bd2a94f1d43e2b95fc9b28ab14c327d91
ashleysalinas <ashleysalinas98@protonmail.com>
2022-01-07 15:12:34 -0600

    html page (might be working?)

--- src/page-template.js
+++ src/page-template.js
@@ -5,7 +5,7 @@ const generateTeam = (team) => {
         <div class="employee-card">
             <div class="card-header">
             <h2>${manager.name}</h2>
-            <h3>${manager.getRole}</h3>
+            <h3>${manager.getRole()}</h3>
             </div>
             <div class="card-body">
             <h4>${manager.officeNumber}</h4>
@@ -20,13 +20,80 @@ const generateTeam = (team) => {
         <div class="employee-card">
             <div class="card-header">
             <h2>${engineer.name}</h2>
-            <h3>${engineer.getRole}</h3>
+            <h3>${engineer.getRole()}</h3>
             </div>
             <div class="card-body">
             <h4>Email:${engineer.email}</h4>
-            <h4>Github: ${engin.github}</h4>
+            <h4>Github: ${engineer.github}</h4>
             </div>
         </div>
         `
     }
+
+    const createInternCard = (intern) => {
+        return `
+        <div class="employee-card">
+            <div class="card-header">
+            <h2>${intern.name}</h2>
+            <h3>${intern.getRole()}</h3>
+            </div>
+            <div class="card-body">
+            <h4>Email:${intern.email}</h4>
+            <h4>Github: ${intern.school}</h4>
+            </div>
+        </div>
+        `
+    }
+
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => generateManager(manager))
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => generateEngineer(engineer))
+        .join("")
+    );
+    html.push(team
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => generateIntern(intern))
+        .join("")
+    );
+
+    return html.join("");
 }
+
+module.exports = team => {
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+</head>
+
+<body>
+    <div>
+        <div>
+            <div>
+                <h1>My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div>
+                ${generateTeam(team)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+};
